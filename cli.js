253cc3d5a96a42c03e548dc7386a7d401564ffe2
Umberto <umberto@ghio.io>
2019-01-08 13:58:18 +0100

    First commit

---
+++ cli.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const config = require(process.env['HOME'] + '/.sssh.json')
+const ipt = require('ipt')
+const out = require('simple-output')
+const { execSync } = require('child_process')
+
+const input = config.map(el => {
+  const name = `${el.name}: ${el.description}`
+  const forward = el.portForward ? `-L ${el.portForward}:127.0.0.1:${el.portForward}` : ''
+  const value = `ssh ${forward} ${el.username}@${el.host}`
+  return { name, value }
+})
+
+// Exits program execution on ESC
+process.stdin.on('keypress', (ch, key) => { key && key.name === 'escape' && process.exit(0) })
+
+const options = {
+  message: 'Select SSH connection',
+  autocomplete: true,
+  multiple: false,
+  size: input.length
+}
+
+out.success('Simple SSH Connection Handler')
+
+// creates interactive interface using ipt
+ipt(input, options).then(command => {
+  out.success('launching ' + command)
+  execSync(`"${command}"`, {
+    cwd: process.cwd(),
+    stdio: [process.stdin, process.stdout, process.stderr]
+  })
+}).catch(err => { out.error(err) })
