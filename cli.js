207bd36b31a63440dde3004130e101055c41b265
Umberto Ghio <ughio@siapcn.it>
2019-12-12 17:02:07 +0100

    Added multiple SSH port forwards

--- cli.js
+++ cli.js
@@ -5,16 +5,16 @@ const ipt = require('ipt')
 const out = require('simple-output')
 const { execSync } = require('child_process')
 
-const input = config.map(el => {
-  const name = `${el.name}: ${el.description}`
-  const forward = el.portForward ? `-L ${el.portForward}:127.0.0.1:${el.portForward}` : ''
-  const key = el.key ? `-i ${el.key}` : ''
-  const value = `ssh ${key} ${forward} ${el.username}@${el.host}`
-  return { name, value }
-})
-
 // Exits program execution on ESC
 process.stdin.on('keypress', (ch, key) => { key && key.name === 'escape' && process.exit(0) })
+
+const forwardParam = ({portForward = false}) => !portForward ? ''
+  : portForward.split(',').map(pf => `-L ${pf.trim()}:127.0.0.1:${pf.trim()}`).join(' ')
+
+const input = config.map((el) => ({
+  name: `${el.name}: ${el.description}`,
+  value: `ssh ${el.key ? `-i ${el.key}` : ''} ${forwardParam(el)} ${el.username}@${el.host}`
+}))
 
 const options = {
   message: 'Select SSH connection',
@@ -23,13 +23,16 @@ const options = {
   size: input.length
 }
 
-out.success('Simple SSH Connection Handler')
-
-// creates interactive interface using ipt
-ipt(input, options).then(command => {
+const executeSsh = (command) => {
   out.success('launching ' + command)
   execSync(`"${command}"`, {
-    cwd: process.cwd(),
-    stdio: [process.stdin, process.stdout, process.stderr]
-  })
-}).catch(err => { out.error(err) })
+      cwd: process.cwd(),  
+      stdio: [process.stdin, process.stdout, process.stderr]
+    })
+}
+
+// creates interactive interface using ipt
+out.success('Simple SSH Connection Handler')
+ipt(input, options)
+  .then(executeSsh)
+  .catch(err => { out.error(err) })
